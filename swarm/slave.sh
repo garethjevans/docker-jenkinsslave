e0290c95470b5db2b601268e01e05830f85f1c02
martin ford <martin.ford@monitise.com>
2016-06-27 13:40:40 +0100

    Allow naming of slaves

--- swarm/slave.sh
+++ swarm/slave.sh
@@ -9,6 +9,7 @@ java -jar /usr/share/jenkins/swarm-client-jar-with-dependencies.jar \
     -password ${JENKINS_PASSWORD:?"Need to set JENKINS_PASSWORD"} \
     -executors ${JENKINS_EXECUTORS:-8} \
     -tunnel :${JENKINS_PORT:-50000} \
+	-name "${JENKINS_NAME:-"Slave"}" \
     -description "${JENKINS_DESCRIPTION:-"Swarm Slave"}" \
     -labels "${JENKINS_LABELS:-"swarm"}" \
     -mode "${JENKINS_MODE:-"normal"}"
